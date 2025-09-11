ad3fd5d53a921b4035a10721b3e1af28e0838616
moatasim <mo32000a@gmail.com>
2025-09-11 16:32:41 +0300

    added contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,39 +1,70 @@
-import React from 'react'
-import MainHeading from '../MainHeading/MainHeading'
+"use client"
+import React, { useEffect, useRef, useState } from 'react'
 import styles from "./contact.module.css"
-import WhatsAppForm from '../WhatsappForm/WhatsappForm'
-import { useTranslations } from 'next-intl'
+import MainHeading from '../MainHeading/MainHeading'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLocationPin } from '@fortawesome/free-solid-svg-icons'
+import SocialUl from '../SocialUl/SocialUl'
+
 const Contact = ({
-  lo
+    lo
 } : {
-  lo: string
+    lo: string
 }) => {
-  const t = useTranslations("HomePage.Contact")
-  return (
-    <section id='contact' className={lo !== "en" ? styles.contact + " " + styles.ar : styles.contact}>
-        
-        <div className="container">
-          <div>
-              
-            <MainHeading>
-            {
-              t("heading")
+
+    const contact: any = useRef(null)
+    const [contactScrolled, setContactScrolled] = useState(false)
+    const scrollContactiSection = () => {
+        if(contact.current){
+            if(contact.current.offsetTop <= (window.scrollY + 500)){
+                setContactScrolled(true)
             }
-            </MainHeading>
-              <div className={styles.form}>
-                <WhatsAppForm lo={lo}/>
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", scrollContactiSection)
+    },[])
+
+    
+    const classNames = [
+        lo === "ar" ? styles.ar : null,
+        contactScrolled ? styles.scrolled : null,
+        styles.contact
+    ] 
+
+  return (
+    <section ref={contact} className={classNames.join(" ")}>
+        <MainHeading>
+            {/* {t("Heading")} */}
+            Contact Us
+        </MainHeading>
+        <div className="container">
+            <div className={styles.info}>
+                <div>
+                    <SocialUl />
+                </div>
+                <div>
+                    <a href="mailto:mohamed.biotech98@gmail.com" target='_blank'>
+                        <span>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </span>
+                        <h4>
+                            mohamed.biotech98@gmail.com
+                        </h4>
+                    </a>
+                </div>
+                <div className={styles.address}>
+                    <span>
+                        <FontAwesomeIcon icon={faLocationPin}></FontAwesomeIcon>
+                    </span>
+                    <address>
+                        City, Austria 
+                    </address>
+                </div>
             </div>
-          </div>
-        <div>
-            <MainHeading>
-              {
-                t("location")
-              }
-              </MainHeading>
             <div className={styles.location}>
-              <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1066.5405006360738!2d39.25722663730846!3d21.43282766257138!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1751987610555!5m2!1sen!2seg" width="600" height="450" style={{ border: "0" }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194347.47826877935!2d-3.844346296540949!3d40.438098610766225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422997800a3c81%3A0xc436dec1618c2269!2sMadrid%2C%20Spain!5e0!3m2!1sen!2seg!4v1738731384484!5m2!1sen!2seg" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
-        </div>
         </div>
     </section>
   )
