b18280545c3df3d28921b1a0402c87325fc248c0
moatasim <mo32000a@gmail.com>
2025-09-16 17:57:38 +0300

    added some dummy content for about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -13,15 +13,76 @@ const About = () => {
         </MainHeading>
         <div className="container">
           <div className={styles.text}>
-            <h2>
-              Dr.in Fairoz Mohammed
-            </h2>
-            <span>
-              Nuklearmidizin und Schilddrüse 
-            </span>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nulla quidem, atque explicabo libero perspiciatis nemo earum harum! Laboriosam deleniti inventore beatae nesciunt quibusdam. Tenetur perferendis placeat asperiores in quaerat.
-            </p>
+            <div className={styles.textHeading}>
+              <h2>
+                Dr.in Fairoz Mohammed
+              </h2>
+              <span>
+                Nuklearmidizin und Schilddrüse 
+              </span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia beatae libero? Dignissimos amet sit molestiae? Veritatis, nulla ducimus, numquam distinctio dicta explicabo, minima asperiores natus quo quis iste eveniet!
+              </p>
+            </div>
+            <div className={styles.textInfo}>
+              <div>
+                <h3>Qualifications</h3>
+                <ul>
+                  <li>
+                    <p>
+                    <span>
+                      2018 - 2023
+                    </span>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. In ad earum ab eos quo libero qui impedit. Aperiam, modi ut id eveniet officiis reiciendis vero
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                    <span>
+                      2018 - 2023
+                    </span>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. In ad earum ab eos quo libero qui impedit. Aperiam, modi ut id eveniet officiis reiciendis vero
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h3>
+                  Certificates
+                </h3>
+                <ul>
+                  <li>
+                    <p>
+                    <span>2024</span>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                    <span>2025</span>
+                      amet consectetur adipisicing elit.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h3>Research</h3>
+                <ul>
+                  <li>
+                    <p>
+                    <span>2022</span>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                    <span>2023</span>
+                      amet consectetur adipisicing elit.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div className={styles.img}>
             <Image src={drImg} alt="Dr.in Fairoz Mohammed"></Image>
