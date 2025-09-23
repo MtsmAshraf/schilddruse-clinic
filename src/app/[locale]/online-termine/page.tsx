4acf1ba644fad567ae6d71ec3785e6016e3d850f
moatasim <mo32000a@gmail.com>
2025-09-23 12:39:44 +0300

    added online-termine page content

--- src/app/[locale]/online-termine/page.tsx
+++ src/app/[locale]/online-termine/page.tsx
@@ -1,8 +1,57 @@
+import MainHeading from '@/components/MainHeading/MainHeading'
 import React from 'react'
+import styles from "./online-termine.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faLocationPin } from '@fortawesome/free-solid-svg-icons'
 
 const OnlineTermine = () => {
   return (
-    <div>OnlineTermine</div>
+    <section className={styles.onlineTermine}>
+      <MainHeading>
+        Online-Termine
+      </MainHeading>
+      <div className="container"> 
+        <h3>
+          Dr. Mohammed Online-Termine
+        </h3>
+        <div className={styles.appts}>
+            <div className={styles.apptOne}>
+                <h4>
+                    Gruppenpraxis Medizin Mariahilf
+                </h4>
+                <p>
+                  <FontAwesomeIcon icon={faClock} />
+                  <span>
+                    Mittwochs : 13:00-19:00 Uhr
+                  </span>
+                </p>
+                <h5>
+                  <FontAwesomeIcon icon={faLocationPin} />
+              <span>
+                    Mariahilfer Straße 95/19, Stiege 1, 1060 Wien (U3 Zieglergasse)
+                  </span>
+                </h5>
+            </div>
+            <div className={styles.apptTwo}>
+                <h4>
+                    SchilddrüsenordinationUbl
+                </h4>
+                <p>
+                  <FontAwesomeIcon icon={faClock} />
+                  <span>
+                    Freitags: 16:00- 19:00 Uhr 
+                  </span>
+                </p>
+                <h5>
+                  <FontAwesomeIcon icon={faLocationPin} />
+                  <span>
+                    Leegasse 2/8, 1140 Wien
+                  </span>
+                </h5>
+            </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
