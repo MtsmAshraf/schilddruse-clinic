df7a76985d4e719e8a7794d9ba512eda6531bdfe
moatasim <mo32000a@gmail.com>
2025-09-24 13:52:47 +0300

    floatingBooking - Calendly => Cal.com

--- src/components/BookBtn/BookBtn.tsx
+++ src/components/BookBtn/BookBtn.tsx
@@ -1,53 +1,41 @@
 "use client"
-// import { useTranslations } from 'next-intl';
 // import React, { useEffect, useState } from 'react'
-// import { PopupButton } from 'react-calendly';
+// import styles from "./"
+// import { PopupButton } from 'react-calendly'
+// import { useTranslations } from 'next-intl'
 
-// const BookBtn = () => {
-//     const t = useTranslations("HomePage")
 
-//   // getting root element on the client side
-  
-//   const [rootElement, setRootElement] = useState<HTMLElement>();
+// const FloatingBooking = () => {
+//     const t = useTranslations
+//     ("HomePage")
 
-//   useEffect(() => {
-//   // Runs only on client
-//   const el: HTMLElement = document.body;
-//   if (el) {
-//       setRootElement(el);
-//   }
-//   }, []); 
-//   return (
-//     <PopupButton
-//         className='book-btn main'
-//         url="https://calendly.com/mo32000a/schilddruse"
-//         /*
-//         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-//         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-//         */
-//         rootElement={rootElement!}
-//         text={`${t("Hero.btns.b1")}`}
-//     />
-//   )
+//     const [rootElement, setRootElement] = useState<HTMLElement>();
+
+//     useEffect(() => {
+//     // Runs only on client
+//     const el: HTMLElement = document.body;
+//     if (el) {
+//         setRootElement(el);
+//     }
+//     }, []); 
+//     return (
+//         <PopupButton
+//             className={styles.floatingBooking}
+//             url="https://calendly.com/mo32000a/schilddruse"
+//             /*
+//             * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+//             * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+//             */
+//             rootElement={rootElement!}
+//             text={`${t("Hero.btns.b1")}`}
+//         />
+// )
 // }
 
-// export default BookBtn
-/* First make sure that you have installed the package */
+// export default FloatingBooking
 
-/* If you are using yarn */
-// yarn add @calcom/embed-react
 
-/* If you are using npm */
-// npm install @calcom/embed-react
-  /* First make sure that you have installed the package */
-
-/* If you are using yarn */
-// yarn add @calcom/embed-react
-
-/* If you are using npm */
-// npm install @calcom/embed-react
-  
-import Cal, { getCalApi } from "@calcom/embed-react";
+import { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 export default function BookBtn() {
   useEffect(() => {
@@ -56,29 +44,13 @@ export default function BookBtn() {
       cal("ui", {"theme":"light","cssVarsPerTheme":{"light":{"cal-brand":"#1197f0"},"dark":{"cal-brand":"#1197f0"}},"hideEventTypeDetails":true,"layout":"month_view"});
     })();
   }, [])
-  
-  useEffect(() => {
-    (async function () {
-      const cal = await getCalApi({"namespace":"follow-up"});
-      cal("ui", {"theme":"dark","cssVarsPerTheme":{"light":{"cal-brand":"#1197f0"},"dark":{"cal-brand":"#1197f0"}},"hideEventTypeDetails":true,"layout":"month_view"});
-    })();
-  }, [])
-  return (
-    <>
-      
-    <Cal namespace="first-visit"
-      calLink="moatasim-ashraf-jez60p/first-visit"
-      style={{width:"100%",height:"100%",overflow:"scroll"}}
-      config={{"layout":"month_view","theme":"light"}}
-      />
-      <Cal namespace="follow-up"
-      calLink="moatasim-ashraf-jez60p/follow-up"
-      style={{width:"100%",height:"100%",overflow:"scroll"}}
-      config={{"layout":"month_view","theme":"dark"}}
-      
-      
-    />;
-    </>
-  )
+  return <button data-cal-namespace="first-visit"
+    data-cal-link="moatasim-ashraf-jez60p/first-visit"
+    className={"book-btn main"}
+    data-cal-config='{"layout":"month_view","theme":"light"}'
+  >
+    Book Appiontment
+  </button>;
 };
+  
   
--- src/components/Termine/Termine.tsx
+++ src/components/Termine/Termine.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock, faLocationPin } from '@fortawesome/free-solid-svg-icons'
 import styles from "./termine.module.css"
 import { Link } from '@/i18n/navigation'
+import BookBtn from '../BookBtn/BookBtn'
 
 const Termine = () => {
   return (
@@ -33,9 +34,7 @@ const Termine = () => {
                         </span>
                     </h5>
                     <div className={styles.btns}>
-                        <Link className='book-btn main' href="/">
-                        Book Appointment
-                        </Link>
+                        <BookBtn />
                     </div>
                     <div className={styles.location}>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.4954545057676!2d16.346496200000004!3d48.197072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d078b9f7daadd%3A0xf4cc95071955a277!2sMedizin%20Mariahilf!5e0!3m2!1sen!2seg!4v1758353796303!5m2!1sen!2seg" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
@@ -59,7 +58,7 @@ const Termine = () => {
                     </h5>
                     <div className={styles.btns}>
                         <a className='book-btn main' href="https://www.meinarztonline.at/app/api/onlineBookingFrame/24205/bookingStep/1" target='_blank'>
-                        Book Appointment
+                            Book Appointment
                         </a>
                     </div>
                     <div className={styles.location}>
