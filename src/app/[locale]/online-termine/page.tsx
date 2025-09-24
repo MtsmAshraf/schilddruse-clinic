fc163d768cfc8c5a843d37ce26dce6c570eb8e60
moatasim <mo32000a@gmail.com>
2025-09-24 12:31:03 +0300

    added clinics locations in Online-Termine paeg

--- src/app/[locale]/online-termine/page.tsx
+++ src/app/[locale]/online-termine/page.tsx
@@ -28,10 +28,18 @@ const OnlineTermine = () => {
                 </p>
                 <h5>
                   <FontAwesomeIcon icon={faLocationPin} />
-              <span>
+                  <span>
                     Mariahilfer Straße 95/19, Stiege 1, 1060 Wien (U3 Zieglergasse)
                   </span>
                 </h5>
+                <div className={styles.btns}>
+                  <a className='book-btn main' href="/">
+                    Book Appointment
+                  </a>
+                </div>
+                <div className={styles.location}>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.4954545057676!2d16.346496200000004!3d48.197072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d078b9f7daadd%3A0xf4cc95071955a277!2sMedizin%20Mariahilf!5e0!3m2!1sen!2seg!4v1758353796303!5m2!1sen!2seg" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
             <div className={styles.apptTwo}>
                 <h4>
@@ -49,11 +57,19 @@ const OnlineTermine = () => {
                     Leegasse 2/8, 1140 Wien
                   </span>
                 </h5>
+                <div className={styles.btns}>
+                  <a className='book-btn main' href="https://www.meinarztonline.at/app/api/onlineBookingFrame/24205/bookingStep/1" target='_blank'>
+                    Book Appointment
+                  </a>
+                </div>
+                <div className={styles.location}>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.8322035616325!2d16.293149900000003!3d48.190584400000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476da70a3284003d%3A0xa760ce3ed98dfa39!2sSchilddr%C3%BCsenordination%20Ubl!5e0!3m2!1sen!2seg!4v1758706179416!5m2!1sen!2seg" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
         </div>
-        <div className={styles.btns + " " + "stagger-text"}>
+        {/* <div className={styles.btns + " " + "stagger-text"}>
             <BookBtn />
-        </div>
+        </div> */}
       </div>
     </section>
   )
