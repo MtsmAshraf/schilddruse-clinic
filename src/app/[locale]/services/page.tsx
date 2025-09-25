5a4dac78e89a23a57a4cc8198ef31c45250594fa
moatasim <mo32000a@gmail.com>
2025-09-25 08:56:28 +0300

    blog link => services ERR FX

--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from "./services.module.css"
 import Services from '@/components/Services/Services'
-import MainHeading from '@/components/MainHeading/MainHeading'
 const ServicesPage = () => {
   return (
         <section className={styles.services}>
