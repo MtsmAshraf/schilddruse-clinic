8e5ac6f42706cec41a4fcb9e812f3710af18fbe5
moatasim <mo32000a@gmail.com>
2025-09-25 18:33:03 +0300

    added services translations ERR FIX

--- src/components/BookBtn/BookBtn.tsx
+++ src/components/BookBtn/BookBtn.tsx
@@ -1,43 +1,5 @@
 "use client"
-// import React, { useEffect, useState } from 'react'
-// import styles from "./"
-// import { PopupButton } from 'react-calendly'
-// import { useTranslations } from 'next-intl'
-
-
-// const FloatingBooking = () => {
-//     const t = useTranslations
-//     ("HomePage")
-
-//     const [rootElement, setRootElement] = useState<HTMLElement>();
-
-//     useEffect(() => {
-//     // Runs only on client
-//     const el: HTMLElement = document.body;
-//     if (el) {
-//         setRootElement(el);
-//     }
-//     }, []); 
-//     return (
-//         <PopupButton
-//             className={styles.floatingBooking}
-//             url="https://calendly.com/mo32000a/schilddruse"
-//             /*
-//             * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-//             * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-//             */
-//             rootElement={rootElement!}
-//             text={`${t("Hero.btns.b1")}`}
-//         />
-// )
-// }
-
-// export default FloatingBooking
-
-
 import { getCalApi } from "@calcom/embed-react";
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "next-intl";
 import { useEffect } from "react";
 export default function BookBtn() {
