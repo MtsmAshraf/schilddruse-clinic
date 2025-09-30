f881448e8a698391cb23c5a08afabcee4e6a9f3a
moatasim <mo32000a@gmail.com>
2025-09-30 09:54:34 +0300

    added popup modal for booking types ERR FX

--- src/components/FloatingBooking/FloatingBooking.tsx
+++ src/components/FloatingBooking/FloatingBooking.tsx
@@ -2,13 +2,10 @@
 import styles from "./floating-booking.module.css"
 
 import BookBtn from "../BookBtn/BookBtn";
-import { useTranslations } from "next-intl";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 export default function FloatingBooking() {
   
-  const t = useTranslations("BookBtn") 
-
   return(
     <Provider store={store}>
       <div className={styles.floatingBooking}>
