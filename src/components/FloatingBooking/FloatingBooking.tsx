8c9c7d7cc96b5c650d94771bea5ddb19b1dfa026
moatasim <mo32000a@gmail.com>
2025-10-16 14:10:49 +0300

    footer note translations edit ERR FX

--- src/components/FloatingBooking/FloatingBooking.tsx
+++ src/components/FloatingBooking/FloatingBooking.tsx
@@ -2,19 +2,20 @@ import { useTranslations } from "next-intl"
 import styles from "./floating-booking.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCalendarDays } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "@/i18n/navigation"
 
 export default function FloatingBooking() {
   
   const t = useTranslations("BookBtn")
   
   return(
-    <a href="/" className={styles.floatingBooking + " " + "book-btn"}>
+    <Link href="/" className={styles.floatingBooking + " " + "book-btn"}>
       <FontAwesomeIcon icon={faCalendarDays} />
       <span>
         {
           t("Text")
         }
       </span>
-    </a>
+    </Link>
   )
 };
--- src/components/Termine/Termine.tsx
+++ src/components/Termine/Termine.tsx
@@ -9,6 +9,7 @@ import Image from 'next/image'
 
 import centerLogo from "../../../public/images/center-logo.png"
 import SocialUl from '../SocialUl/SocialUl'
+import Link from 'next/link'
 
 const Termine = () => {
 
@@ -122,14 +123,14 @@ const Termine = () => {
                         </span>
                     </h5>
                     <div className={styles.btns}>
-                        <a href="/" className='book-btn'>
+                        <Link href="/" className='book-btn'>
                             <FontAwesomeIcon icon={faCalendarDays} />
                             <span>
                                 {
                                     t("Book")
                                 }
                             </span>
-                        </a>
+                        </Link>
                     </div>
                     <div className={styles.location}>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.4954545057676!2d16.346496200000004!3d48.197072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d078b9f7daadd%3A0xf4cc95071955a277!2sMedizin%20Mariahilf!5e0!3m2!1sen!2seg!4v1758353796303!5m2!1sen!2seg" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
