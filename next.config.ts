415c37b4fced45968362879576696340dd90cb44
moatasim <mo32000a@gmail.com>
2025-12-25 12:49:06 +0200

    increased the caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    async headers() {
+    return [{
+        source: '/:path*',
+        headers: [
+            {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, stale-while-revalidate=86400'
+            }
+        ],
+        images: {
+            formats: ['image/avif', 'image/webp'],
+            minimumCacheTTL: 86400, // Cache images for 24 hours
+        }
+    }];
+    
+  }
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
